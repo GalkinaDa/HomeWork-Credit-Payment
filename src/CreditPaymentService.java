6d12dcf713b9eee15282f440e5389baf9c0d018e
Galkina <darja.galkin@gmail.com>
2025-04-20 21:00:17 +0300

    Сalculate for 12 months

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,20 @@
+public class CreditPaymentService {
+
+    public int calculate(int a, double b, int c) {
+
+        int loanAmount = a;
+        double percent = b;
+        int term = c;
+
+        // месячная процентная ставка
+        double m = b / 100 / 12 ;
+
+        // коэффициент аннуитета
+        double k = (m * Math.pow((1 + m), c)) / (Math.pow((1 + m), c) - 1);
+
+        double result = a * k;
+        return (int) result;
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        CreditPaymentService service = new CreditPaymentService();
+
+        // сумма кредита в руб.
+        int loanAmount = 1_000_000;
+
+        // процентная ставка
+        double percent = 9.99;
+
+        // срок кредита в мес.
+        int term = 12;
+
+        int annuityPayment = service.calculate(loanAmount, percent, term);
+        System.out.println(annuityPayment);
+    }
+}
